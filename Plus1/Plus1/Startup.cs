863a797410a899965d7dcca1abd60b216e1212be
roel komen <roel24komen@gmail.com>
2018-06-05 12:57:41 +0200

    admin auth admin area

--- Plus1/Plus1/Startup.cs
+++ Plus1/Plus1/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Plus1.Models;
 
 [assembly: OwinStartupAttribute(typeof(Plus1.Startup))]
 namespace Plus1
@@ -9,6 +12,30 @@ namespace Plus1
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
+        }
+        private void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Bezoeker"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Bezoeker";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Webredacteur"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Webredacteur";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
         }
     }
 }
