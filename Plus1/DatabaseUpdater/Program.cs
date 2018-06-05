ca2c594b4a706406f6c6a614e2902488346d2806
Tom Zwikker <tom.zwikker@gmail.com>
2018-06-05 12:22:37 +0200

    Created DatabaseUpdater

---
+++ Plus1/DatabaseUpdater/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DatabaseUpdater
+{
+    class Program
+    {
+        static string BaseURL = "http://supermaco.starwave.nl/api/";
+
+        static void Main(string[] args)
+        {
+            UpdateProductTable(); 
+        }
+
+        static void UpdateProductTable()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load( BaseURL + "products");
+            string xmlcontents = doc.InnerXml;
+
+            XmlElement xelRoot = doc.DocumentElement;
+            XmlNodeList xnlNodes = xelRoot.SelectNodes("/Products/Product");
+
+            foreach (XmlNode xndNode in xnlNodes)
+            {
+                Console.WriteLine(xndNode["Title"].InnerText);
+ 
+
+                //Your sql insert command will go here;
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
