74dc27022273ded88fb7f74d49e4766e682e7937
Koen Wokke <koen.wok@live.nl>
2018-06-06 21:14:07 +0200

    Layout, ProductImage, Productsoverview

--- Plus1/Plus1/Controllers/HomeController.cs
+++ Plus1/Plus1/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using System.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Plus1.Models;
 
 namespace Plus1.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Products.ToList());
         }
 
         public ActionResult About()
