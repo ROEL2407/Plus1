176008136c070d6f786d49423d12b6dc886fe353
Koen Wokke <koen.wok@live.nl>
2018-06-19 11:00:54 +0200

    ignoted fix

---
+++ Plus1/Plus1/Controllers/NewsArticles1Controller.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Plus1.Models;
+
+
+namespace Plus1.Controllers
+{
+    public class NewsArticles1Controller : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: NewsArticles1
+        public ActionResult Index()
+        {
+            return View(db.NewsArticles.ToList());
+        }
+
+        // GET: NewsArticles1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NewsArticle newsArticle = db.NewsArticles.Find(id);
+            if (newsArticle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(newsArticle);
+        }
+
+        //hallo
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
