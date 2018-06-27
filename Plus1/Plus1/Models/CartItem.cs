132ddbca1ae101d31b46d63d9b2975dfabe3c4d5
Tom Zwikker <tom.zwikker@gmail.com>
2018-06-27 10:47:04 +0200

    Cart Item Model

--- Plus1/Plus1/Models/CartItem.cs
+++ Plus1/Plus1/Models/CartItem.cs
@@ -8,13 +8,8 @@ namespace Plus1.Models
 {
     public class CartItem
     {
-<<<<<<< HEAD
         [Key]
         public int CartItemID { get; set; }
-=======
-       // [Key]
-        public int CartItemID { get; set; } //Groeten Koen. Anders was er geen 'key'
->>>>>>> 27a0f223759b35e42ccf5c3546958b4448c2e107
         public Cart CartID { get; set; }
         public string EAN { get; set; }
         public int Quantity { get; set; }
