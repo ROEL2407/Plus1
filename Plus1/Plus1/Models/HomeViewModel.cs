c5f40935af8cd933bcbdcd99dea81c9ea4046793
Koen Wokke <koen.wok@live.nl>
2018-06-27 10:55:11 +0200

    shit

---
+++ Plus1/Plus1/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plus1.Models
+{
+
+    public class HomeViewModel
+    {
+        public ICollection<Product> Products { get; set; }
+        public ICollection<Promotion> Promotions { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
