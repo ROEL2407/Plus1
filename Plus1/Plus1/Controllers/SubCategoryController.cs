1bfd22367a73de3e964def3c0bf92d61b59960ad
Tom Zwikker <tom.zwikker@gmail.com>
2018-07-02 10:52:14 +0200

    Controllers

---
+++ Plus1/Plus1/Controllers/SubCategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Plus1.Models;
+
+namespace Plus1.Controllers
+{
+    public class SubCategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SubCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string id)
+        {
+            SubCategory Subcategory = db.SubCategory.Find(id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (Subcategory == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new CategoryViewModel();
+            List<SubCategory> Cats = new List<SubCategory>();
+            Cats.Add(Subcategory);
+            viewModel.SubCategory = Cats;
+            viewModel.Products = db.Products.Take(100).ToList();
+            viewModel.SubSubCategory = db.SubSubCategory.Where(c => c.ParentName == Subcategory.Name).Take(20).ToList();
+            return View(viewModel);
+        }
+    }
+}
---
+++ Plus1/Plus1/Controllers/SubSubCategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Plus1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plus1.Controllers
+{
+    public class SubSubCategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SubSubCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string id)
+        {
+            SubSubCategory SubSubcategory = db.SubSubCategory.Find(id);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (SubSubcategory == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new CategoryViewModel();
+            List<SubSubCategory> Cats = new List<SubSubCategory>();
+            Cats.Add(SubSubcategory);
+            viewModel.SubSubCategory = Cats;
+            viewModel.Products = db.Products.Where(c => c.SubSubcategory == SubSubcategory.Name).Take(100).ToList();
+            return View(viewModel);
+        }
+    }
+}
