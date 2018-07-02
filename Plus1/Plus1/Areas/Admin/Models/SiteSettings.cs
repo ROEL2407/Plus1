478f07a9271c149cab15b5a8a86cbe66e5f5d692
Koen Wokke <koen.wok@live.nl>
2018-07-02 11:15:06 +0200

    try

---
+++ Plus1/Plus1/Areas/Admin/Models/SiteSettings.cs
@@ -0,0 +1,14 @@
+﻿using Plus1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plus1.Areas.Admin.Models
+{
+    public class SiteSettings
+    {
+        public Product EAN { get; set; }
+        public bool Visible { get; set; }
+    }
+}
