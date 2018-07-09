e1a5102dadc00be364c129b2531dd4c6d9316e09
Tom Zwikker <tom.zwikker@gmail.com>
2018-07-09 10:49:05 +0200

    Order Controller

---
+++ Plus1/Plus1/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plus1.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Order
+        public ActionResult Delivery()
+        {
+            return View();
+        }
+    }
+}
